955313733d82f2c6a2b1a529fa82e4b5d4b8b623
Rahul Gopinath <rahul@gopinath.org>
2020-04-04 10:44:52 +0200

    update

--- EarleyParser/src/main/java/parser/App.java
+++ EarleyParser/src/main/java/parser/App.java
@@ -1,13 +1,14 @@
 package parser;
 
 /**
- * Hello world!
+ * Earley Parser
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        ParserLib pl = new ParserLib(args[0]);
+        ParseTree result = pl.parse_text(args[1]);
+        pl.show_tree(result);
     }
 }
