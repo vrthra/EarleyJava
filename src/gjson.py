62a0eeef49a87e63f4a9689dc19d1d5e6093fc2d
Rahul Gopinath <rahul@gopinath.org>
2020-04-05 09:00:05 +0200

    jsongrammar

---
+++ src/gjson.py
@@ -0,0 +1,102 @@
+import json
+json_grammar = { "<start>": [
+    ["<json>"]
+    ],
+    "<json>": [
+        ["<element>"]
+        ],
+    "<value>": [
+        ["<object>"],
+        ["<array>"],
+        ["<string>"],
+        ["<number>"],
+        ["true"],
+        ["false"],
+        ["null"]
+        ],
+    "<object>": [
+        ["{", "<ws>", "}"],
+        ["{", "<members>", "}"]
+        ],
+    "<members>": [
+        ["<member>"],
+        ["<member>", "<members>"]
+        ],
+    "<member>": [
+        ["<ws>", "<string>", "<ws>", ":", "<element>"]
+        ],
+    "<array>": [
+        ["[", "<ws>", "]"],
+        ["[", "<elements>", "]"]
+        ],
+    "<elements>": [
+        ["<element>"],
+        ["<element>", "<elements>"]
+        ],
+    "<element>": [
+        ["<ws>", "<value>", "<ws>"]
+        ],
+    "<string>": [
+        ["\"", "<characters>", "\""]
+        ],
+    "<characters>": [
+        [""],
+        ["<character>", "<characters>"]],
+    "<character>": ([[chr(i)] for i in range(0x0020,0x10FFFF) if chr(i) not in {'"', '\\'}] + [["\\", "<escape>"]]),
+    "<escape>": [
+        ["\""],
+        ["\\"],
+        ["/"],
+        ["b"],
+        ["f"],
+        ["n"],
+        ["r"],
+        ["t"],
+        ["u", "<hex>", "<hex>", "<hex>", "<hex>"],
+        ],
+    "<hex>": ([["<digit>"]] + 
+            [[chr(i)] for i in range(ord('A'), ord('F') + 1)] + 
+            [[chr(i)] for i in range(ord('a'), ord('f') + 1)]),
+    "<number>": [
+            ["<integer>", "<fraction>", "<exponent>"]
+            ],
+    "<integer>": [
+            ["<digit>"],
+            ["<onenine>", "<digits>"],
+            ["-", "<digits>"],
+            ["-", "<onenine>", "<digits>"]
+            ],
+    "<digits>": [
+            ["<digit>"],
+            ["<digit>", "<digits>"]],
+    "<digit>": [
+            ["0"],
+            ["<onenine>"]
+            ],
+    "<onenine>": [
+            ["1"], ["2"], ["3"], ["4"], ["5"], ["6"], ["7"], ["8"], ["9"]],
+    "<fraction>": [
+            [""],
+            [".", "<digits>"]
+            ],
+    "<exponent>": [
+            [""],
+            ["E", "<sign>", "<digits>"],
+            ["e", "<sign>", "<digits>"]
+            ],
+    "<sign>": [
+            [""],
+            ["+"],
+            ["-"]
+            ],
+    "<ws>": [
+            [""],
+            ["<sp1>", "<ws>"]],
+    "<sp1>": [
+            [chr(0x0020)],
+            [chr(0x000A)],
+            [chr(0x000D)],
+            [chr(0x0009)]]
+    }
+
+print(json.dumps(json_grammar, indent=4))
